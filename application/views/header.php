ce4bc11f49e499705efa06c45e4ec0a3a8bf62c0
M05 <pre.mos_os@hotmail.com>
2016-09-19 21:20:56 +0700

    master

---
+++ application/views/header.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?=base_url();?>assets/css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="<?=base_url();?>assets/css/bootstrap.css"/>
+	<link rel="stylesheet" href="<?=base_url();?>assets/css/stylesheet.css"/>
+	<link rel="stylesheet" href="<?=base_url();?>assets/css/animate.css"/>
+	<link rel="stylesheet" href="<?=base_url();?>assets/css/pnotify.min.css"/>
+	<link rel="stylesheet" href="<?=base_url();?>assets/css/sweetalert.min.css"/>
+	<link rel="stylesheet" href="<?=base_url();?>assets/css/summernote.css"/>
+</head>
+<body>
